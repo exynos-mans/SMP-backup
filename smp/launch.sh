35522aeaa625c791cf64427c1adc750ba7a721af
exynos-mans <devj8223@gmail.com>
2022-06-26 10:50:20 +0000

    treewide: dockerfile startup

--- smp/launch.sh
+++ smp/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export JAVA_HOME=/workspace/workspace-images/tools/jdk-16.0.2+7
-export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:"$PATH"
 
 java -Xmx8144M -Xms8144M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper*.jar
 
