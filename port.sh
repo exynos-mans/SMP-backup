9fb5c5d52146055c447aa07a53103d0863220c57
exynos-mans <devj8223@gmail.com>
2022-07-04 11:42:37 +0000

    Mon 04 Jul 2022 11:42:36 AM UTC

--- port.sh
+++ port.sh
@@ -26,3 +26,13 @@ msg_content=\"$message2\"
 ## discord webhook
 url='https://discord.com/api/webhooks/823109852662071317/IHX5Horiqpktu740QrXWkVvE2K5bv-HMhpIprVDoYQXPIcStSHdhcJ1kT2a7LoyG5Pyu'
 curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url
+
+# send message (dynmap)
+message4="Dynamic Map URL -- $(gp url 8123)"
+## format to parse to curl
+## echo Sending message: $message
+msg_content=\"$message4\"
+
+## discord webhook
+url='https://discord.com/api/webhooks/823109852662071317/IHX5Horiqpktu740QrXWkVvE2K5bv-HMhpIprVDoYQXPIcStSHdhcJ1kT2a7LoyG5Pyu'
+curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url
